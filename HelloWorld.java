5b118d90f4e85f274439e63f9fa02c9e1bbe9971
JaykomGIT <83836725+JaykomGIT@users.noreply.github.com>
2026-03-20 11:07:06 -0400

    Rename variable and add new string

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        String  hello_world = "Hello, World!";
+        String HelloWorld = "hello_world!"; 
+        String hello_world1 = "Hello, World!";
         System.out.println(hello_world);
     }
 }
